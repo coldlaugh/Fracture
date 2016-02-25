e8b179c64b5e6c87d8bf9fa911f3fadcde493eea
Leyou <leyou@umich.edu>
2016-02-24 22:27:17 -0500

    modified:   diff.f 	modified:   relax.f

--- diff.f
+++ diff.f
@@ -7,12 +7,11 @@ C      spring energy, spring const k,  and bending energy, const kappa
       double precision, dimension(0:N-1,0:2) ::k
       double precision ::strain,kappa
       double precision :: E
-C      f2py intent(in,out,inplace) :: E
       integer :: i,j,i1,j1,m,m0,m1,m2,ll,m3,m4,m5,im1,jm1
       double precision :: l0,l1,l2,dx,dy,c,c0,c1,eps,dpp
 	  double precision dxm,dym,l3,l4,l5,theta
       double precision, dimension(0:N-1) :: ux,uy
-      double precision :: Ebend 
+      double precision :: Ebend
 
       c0=sqrt(3.)/2.
       c=c0*(1.+strain)*dble(L-1)
@@ -31,7 +30,7 @@ C      f2py intent(in,out,inplace) :: E
          E=E+1.d3*(v(m+N)-dble(j)*c)*(v(m+N)-dble(j)*c)
       enddo
       enddo
-  
+
       do i=0,L-1
       do j=0,L-1
         m=i+j*L
@@ -54,7 +53,7 @@ C      f2py intent(in,out,inplace) :: E
            dx=ux(m0)-ux(m)
            dy=uy(m0)-uy(m)
 C           dx=dx +0.5*dble(L)*dnint(dy/c1)
-           dx=dx - L * dnint(dx/L)           
+           dx=dx - L * dnint(dx/L)
 C           dy= dy - c1*dnint(dy/c1)
            l0=sqrt(dx*dx+dy*dy)
            E=E+k(m,0)*((l0)-1.)**2
@@ -62,7 +61,7 @@ C           dy= dy - c1*dnint(dy/c1)
               dxm=ux(m)-ux(m3)
               dym=uy(m)-uy(m3)
 C              dxm=dxm +0.5*dble(L)*dnint(dym/c1)
-              dxm=dxm - L * dnint(dxm/L)              
+              dxm=dxm - L * dnint(dxm/L)
 C              dym=dym - c1 * dnint(dym/c1)
               l3=sqrt(dxm*dxm+dym*dym)
               dpp=min((dxm*dx+dym*dy)/(l0*l3),1.d0)
@@ -75,7 +74,7 @@ C              dym=dym - c1 * dnint(dym/c1)
            dx=ux(m1)-ux(m)
            dy=uy(m1)-uy(m)
 C           dx=dx +0.5*dble(L)*dnint(dy/c1)
-           dx=dx - L * dnint(dx/L)           
+           dx=dx - L * dnint(dx/L)
 C          added rounding by leyou
 C           dy= dy - c1*dnint(dy/c1)
            l1=sqrt(dx*dx+dy*dy)
@@ -84,12 +83,12 @@ C           dy= dy - c1*dnint(dy/c1)
               dxm=ux(m)-ux(m4)
               dym=uy(m)-uy(m4)
 C              dxm=dxm +0.5*dble(L)*dnint(dym/c1)
-              dxm=dxm - L * dnint(dxm/L)              
+              dxm=dxm - L * dnint(dxm/L)
 C              dym= dym - c1*dnint(dym/c1)
               l4=sqrt(dxm*dxm+dym*dym)
               dpp=min((dxm*dx+dym*dy)/(l1*l4),1.d0)
               theta=dacos(dpp)
-             
+
               E=E+kappa*theta*theta
            endif
         endif
@@ -98,7 +97,7 @@ C              dym= dym - c1*dnint(dym/c1)
            dx=ux(m2)-ux(m)
            dy=uy(m2)-uy(m)
 C           dx=dx +0.5*dble(L)*dnint(dy/c1)
-           dx=dx - L * dnint(dx/L)           
+           dx=dx - L * dnint(dx/L)
 C          added rounding by leyou
 C           dy= dy - c1*dnint(dy/c1)
            l2=sqrt(dx*dx+dy*dy)
@@ -107,12 +106,12 @@ C           dy= dy - c1*dnint(dy/c1)
               dxm=ux(m)-ux(m5)
               dym=uy(m)-uy(m5)
 C              dxm=dxm +0.5*dble(L)*dnint(dym/c1)
-              dxm=dxm - L * dnint(dxm/L)         
+              dxm=dxm - L * dnint(dxm/L)
 C              dym= dym - c1*dnint(dym/c1)
               l5=sqrt(dxm*dxm+dym*dym)
               dpp=min((dxm*dx+dym*dy)/(l2*l5),1.d0)
               theta=dacos(dpp)
-              
+
               E=E+kappa*theta*theta
            endif
         endif
@@ -124,7 +123,7 @@ C              dym= dym - c1*dnint(dym/c1)
          	E=E+1.d3*(v(m+N)-(dble(j)*c))**2
       	enddo
       enddo
-	  
+
       E=E/2.
       end subroutine Energy
 
@@ -155,7 +154,7 @@ C              dym= dym - c1*dnint(dym/c1)
            ux(m)=v(m)
            uy(m)=v(m+N)
         enddo
-  
+
         do j=0,L-1
         do i=0,L-1
 C           !i=mod(m,L)
@@ -186,7 +185,7 @@ C           !j=(m/L)
               dxp=ux(m0)-ux(m)
               dyp=uy(m0)-uy(m)
 C              dxp=dxp +0.5*dble(L)*dnint(dyp/c1)
-              dxp=dxp - L * dnint(dxp/L)              
+              dxp=dxp - L * dnint(dxp/L)
 C              dyp= dyp - c1*dnint(dyp/c1)
               lp=sqrt(dxp*dxp+dyp*dyp)
               dEE=k(m,0)*(lp-1.)/lp
@@ -201,7 +200,7 @@ C              dyp= dyp - c1*dnint(dyp/c1)
                  dxm=ux(m)-ux(m3)
                  dym=uy(m)-uy(m3)
 C                 dxm=dxm +0.5*dble(L)*dnint(dym/c1)
-                 dxm=dxm - L * dnint(dxm/L)                 
+                 dxm=dxm - L * dnint(dxm/L)
 C                 dym= dym-c1*dnint(dym/c1)
                  lm=sqrt(dxm*dxm+dym*dym)
                  dott=(dxm*dxp+dym*dyp)
@@ -210,14 +209,14 @@ C                 dym= dym-c1*dnint(dym/c1)
                     dpp=min(dott/lp/lm,1.d0)
                     theta=dacos(dpp)
                     dEbx1= -theta*kappa*( (dxp)/lp/lm+
-     +                      (-dxm/lm**3/lp)*dott )/den 
+     +                      (-dxm/lm**3/lp)*dott )/den
                     dEbx2= -theta*kappa*( (-dxm)/lp/lm+
      +                      (dxp/lp**3/lm)*dott )/den
                     dEx=dEx +dEbx1+dEbx2
                     dEby1= -theta*kappa*( (dyp)/lp/lm+
-     +                      (-dym/lm**3/lp)*dott )/den     
+     +                      (-dym/lm**3/lp)*dott )/den
                     dEby2= -theta*kappa*( (-dym)/lp/lm+
-     +                      (dyp/lp**3/lm)*dott )/den            
+     +                      (dyp/lp**3/lm)*dott )/den
                     dEy=dEy+dEby1+dEby2
                     dE(m3) = dE(m3) - dEbx1
                     dE(m3+N) = dE(m3+N) - dEby1
@@ -257,7 +256,7 @@ C              dyp= dyp - c1*dnint(dyp/c1)
                  dym=uy(m)-uy(m4)
 C                 dxm=dxm +0.5*dble(L)*dnint(dym/c1)
                  dxm=dxm - L * dnint(dxm/L)
-                 
+
 C                 dym= dym-c1*dnint(dym/c1)
                  lm=sqrt(dxm*dxm+dym*dym)
                  dott=(dxm*dxp+dym*dyp)
@@ -266,14 +265,14 @@ C                 dym= dym-c1*dnint(dym/c1)
                     dpp=min(dott/lp/lm,1.d0)
                     theta=dacos(dpp)
                     dEbx1= -theta*kappa*( (dxp)/lp/lm+
-     +                      (-dxm/lm**3/lp)*dott )/den 
+     +                      (-dxm/lm**3/lp)*dott )/den
                     dEbx2= -theta*kappa*( (-dxm)/lp/lm+
      +                      (dxp/lp**3/lm)*dott )/den
                     dEx=dEx +dEbx1+dEbx2
                     dEby1= -theta*kappa*( (dyp)/lp/lm+
-     +                      (-dym/lm**3/lp)*dott )/den     
+     +                      (-dym/lm**3/lp)*dott )/den
                     dEby2= -theta*kappa*( (-dym)/lp/lm+
-     +                      (dyp/lp**3/lm)*dott )/den            
+     +                      (dyp/lp**3/lm)*dott )/den
                     dEy=dEy+dEby1+dEby2
 
                     dE(m4) = dE(m4) - dEbx1
@@ -296,7 +295,7 @@ C                 dym= dym-c1*dnint(dym/c1)
               dxp=ux(m2)-ux(m)
               dyp=uy(m2)-uy(m)
 C              dxp=dxp +0.5*dble(L)*dnint(dyp/c1)
-              dxp=dxp - L * dnint(dxp/L)              
+              dxp=dxp - L * dnint(dxp/L)
 C              dyp= dyp - c1*dnint(dyp/c1)
               lp=sqrt(dxp*dxp+dyp*dyp)
               dEE=k(m,2)*(lp-1.)/lp
@@ -311,7 +310,7 @@ C              dyp= dyp - c1*dnint(dyp/c1)
                  dxm=ux(m)-ux(m5)
                  dym=uy(m)-uy(m5)
 C                 dxm=dxm +0.5*dble(L)*dnint(dym/c1)
-                 dxm=dxm - L * dnint(dxm/L)          
+                 dxm=dxm - L * dnint(dxm/L)
 C                 dym= dym-c1*dnint(dym/c1)
                  lm=sqrt(dxm*dxm+dym*dym)
                  dott=(dxm*dxp+dym*dyp)
@@ -320,14 +319,14 @@ C                 dym= dym-c1*dnint(dym/c1)
                     dpp=min(dott/lp/lm,1.d0)
                     theta=dacos(dpp)
                     dEbx1= -theta*kappa*( (dxp)/lp/lm+
-     +                      (-dxm/lm**3/lp)*dott )/den 
+     +                      (-dxm/lm**3/lp)*dott )/den
                     dEbx2= -theta*kappa*( (-dxm)/lp/lm+
      +                      (dxp/lp**3/lm)*dott )/den
                     dEx=dEx +dEbx1+dEbx2
                     dEby1= -theta*kappa*( (dyp)/lp/lm+
-     +                      (-dym/lm**3/lp)*dott )/den     
+     +                      (-dym/lm**3/lp)*dott )/den
                     dEby2= -theta*kappa*( (-dym)/lp/lm+
-     +                      (dyp/lp**3/lm)*dott )/den            
+     +                      (dyp/lp**3/lm)*dott )/den
                     dEy=dEy+dEby1+dEby2
 
                     dE(m5) = dE(m5) - dEbx1
@@ -351,8 +350,8 @@ C                 dym= dym-c1*dnint(dym/c1)
               dxp=ux(m3)-ux(m)
               dyp=uy(m3)-uy(m)
 C              dxp=dxp +0.5*dble(L)*dnint(dyp/c1)
-              dxp=dxp - L * dnint(dxp/L)            
-C              dyp= dyp - c1*dnint(dyp/c1)     
+              dxp=dxp - L * dnint(dxp/L)
+C              dyp= dyp - c1*dnint(dyp/c1)
               lp=sqrt(dxp*dxp+dyp*dyp)
               dEE=k(m3,0)*(lp-1.)/lp
               dEx=dEx-dxp*dEE
@@ -368,8 +367,8 @@ C              dyp= dyp - c1*dnint(dyp/c1)
               dxp=ux(m4)-ux(m)
               dyp=uy(m4)-uy(m)
 C              dxp=dxp +0.5*dble(L)*dnint(dyp/c1)
-              dxp=dxp - L * dnint(dxp/L)              
-C              dyp= dyp - c1*dnint(dyp/c1)     
+              dxp=dxp - L * dnint(dxp/L)
+C              dyp= dyp - c1*dnint(dyp/c1)
               lp=sqrt(dxp*dxp+dyp*dyp)
               dEE=k(m4,1)*(lp-1.)/(lp)
               dEx=dEx-dxp*dEE
@@ -386,8 +385,8 @@ C              dyp= dyp - c1*dnint(dyp/c1)
               dxp=ux(m5)-ux(m)
               dyp=uy(m5)-uy(m)
 C              dxp=dxp +0.5*dble(L)*dnint(dyp/c1)
-              dxp=dxp - L * dnint(dxp/L)          
-C              dyp= dyp - c1*dnint(dyp/c1)     
+              dxp=dxp - L * dnint(dxp/L)
+C              dyp= dyp - c1*dnint(dyp/c1)
               lp=sqrt(dxp*dxp+dyp*dyp)
               dEE=k(m5,2)*(lp-1.)/lp
               dEx=dEx-dxp*dEE
@@ -425,7 +424,7 @@ C              dyp= dyp - c1*dnint(dyp/c1)
         integer, intent(in) ::L
         real(8), intent(in), dimension(0:N-1,0:2) ::k
         real(8), intent(in) ::strain
-        real(8), intent(inout), dimension(0:N-1,0:2) ::lengths  
+        real(8), intent(inout), dimension(0:N-1,0:2) ::lengths
         integer :: i,j,i1,j1,m,m0,m1,m2,ll
         real(8) :: l0,l1,l2,dx,dy,c,c0,c1
         real(8), dimension(0:N-1) :: ux,uy
@@ -438,7 +437,7 @@ C              dyp= dyp - c1*dnint(dyp/c1)
            ux(m)=v(m)
            uy(m)=v(m+N)
         enddo
-  
+
         do m = 0, N-1
            i=mod(m,L)
            j=(m/L)
@@ -450,23 +449,159 @@ C              dyp= dyp - c1*dnint(dyp/c1)
            m2=i1+j1*L
            dx=ux(m0)-ux(m)
            dy=uy(m0)-uy(m)
-           dx=dx - L * dnint(dx/L)           
+           dx=dx - L * dnint(dx/L)
            l0=dsqrt(dx*dx+dy*dy)
            lengths(m,0)=l0*k(m,0)
            dx=ux(m1)-ux(m)
            dy=uy(m1)-uy(m)
-           dx=dx - L * dnint(dx/L)     
+           dx=dx - L * dnint(dx/L)
            l1=dsqrt(dx*dx+dy*dy)
            lengths(m,1)=l1*k(m,1)
            dx=ux(m2)-ux(m)
-           dy=uy(m2)-uy(m) 
-           dx=dx - L * dnint(dx/L)  
+           dy=uy(m2)-uy(m)
+           dx=dx - L * dnint(dx/L)
            l2=dsqrt(dx*dx+dy*dy)
            lengths(m,2)=l2*k(m,2)
         enddo
 
-      end subroutine bonds 
+      end subroutine bonds
 
-    
-     
-  
+      subroutine singleDEnergy(v,k,L,N,nsx,nsy,dEx,dEy)
+        implicit none
+        integer, intent(in)::N
+        real(8), intent(inout), dimension(0:2*N-1) :: v
+        integer, intent(in) ::L
+        real(8), intent(in), dimension(0:N-1,0:2) ::k
+        real(8), intent(inout) :: dEx,dEy
+        integer :: nsx,nsy
+        integer :: i,j,i1,j1,im1,jm1,m,m0,m1,m2,m3,m4,m5,ll
+        real(8) :: dxp,dyp,dEE,c0,c,c1,eps,den,dxm,dym
+        real(8)::  dpp,theta,dott,lp,lm,dEbx1,dEby1,dEbx2,dEby2
+
+        real(8), dimension(0:N-1) :: ux,uy
+
+        c0=sqrt(3.)/2.
+        eps = 0.01
+        ll=L*(L-1)
+
+        i = mod(nsx,L)
+        j = int(nsx/L)
+
+        if ( j .eq. 0) then
+            dEx = 0.d0
+            dEy = 0.d0
+            return
+        endif
+
+
+        m=i+j*L
+        i1=i+1
+        if (i1==L) i1=0
+        j1=j+1
+        if (j1==L) j1=0
+        im1=i-1
+        if (im1.lt.0) im1=L-1
+        jm1=j-1
+        if (jm1.lt.0) jm1=L-1
+
+
+        m0=i1+j*L
+        m1=i+j1*L
+        m2=i1+j1*L
+        m3=im1+j*L
+        m4=i+jm1*L
+        m5=im1+jm1*L
+
+
+        dEx=0.
+        dEy=0.
+        lp=1.
+           if (k(m,0).gt.eps) then
+              dxp=v(m0)-v(m)
+              dyp=v(m0+N)-v(m+N)
+              dxp=dxp - L * dnint(dxp/L)
+              lp=sqrt(dxp*dxp+dyp*dyp)
+              dEE=k(m,0)*(lp-1.)/lp
+              dEx=dEx-dxp*dEE
+              dEy=dEy-dyp*dEE
+           endif
+
+           lp=1.
+           if (k(m,1).gt.eps) then
+              dxp=v(m1)-v(m)
+              dyp=v(m1+N)-v(m+N)
+              dxp=dxp - L * dnint(dxp/L)
+              lp=sqrt(dxp*dxp+dyp*dyp)
+              dEE=k(m,1)*(lp-1.)/lp
+              dEx=dEx-dxp*dEE
+              dEy=dEy-dyp*dEE
+           endif
+
+           lp=1.
+           if (k(m,2).gt.eps) then
+              dxp=v(m2)-v(m)
+              dyp=v(m2+N)-v(m+N)
+              dxp=dxp - L * dnint(dxp/L)
+              lp=sqrt(dxp*dxp+dyp*dyp)
+              dEE=k(m,2)*(lp-1.)/lp
+              dEx=dEx-dxp*dEE
+              dEy=dEy-dyp*dEE
+           endif
+
+           lp=1.
+           if (k(m3,0).gt.eps) then
+              dxp=v(m3)-v(m)
+              dyp=v(m3)-v(m+N)
+              dxp=dxp - L * dnint(dxp/L)
+              lp=sqrt(dxp*dxp+dyp*dyp)
+              dEE=k(m3,0)*(lp-1.)/lp
+              dEx=dEx-dxp*dEE
+              dEy=dEy-dyp*dEE
+           endif
+
+           lp=1.
+           if (k(m4,1).gt.eps) then
+              dxp=v(m4)-v(m)
+              dyp=v(m4+N)-v(m+N)
+              dxp=dxp - L * dnint(dxp/L)
+              lp=sqrt(dxp*dxp+dyp*dyp)
+              dEE=k(m4,1)*(lp-1.)/(lp)
+              dEx=dEx-dxp*dEE
+              dEy=dEy-dyp*dEE
+           endif
+
+           lp=1.
+           if (k(m5,2).gt.eps) then
+              dxp=v(m5)-v(m)
+              dyp=v(m5+N)-v(m+N)
+              dxp=dxp - L * dnint(dxp/L)
+              lp=sqrt(dxp*dxp+dyp*dyp)
+              dEE=k(m5,2)*(lp-1.)/lp
+              dEx=dEx-dxp*dEE
+              dEy=dEy-dyp*dEE
+           endif
+        enddo
+        enddo
+
+        if (j .eq. (L-1)) then
+            dEy = 0.d0
+            return
+        endif
+
+      end subroutine singleDEnergy
+
+      subroutine moveBoundary(v,L,strain,N)
+          real(8), intent(inout), dimension(0:2*N-1) :: v
+          integer, intent(in) ::L
+          double precision ::strain
+          integer ::N
+          integer ::i,j,m
+          double precision::c
+          c=0.5d0*sqrt(3)*(1.+strain)*(L-1)
+          do i=0,L-1
+          do j=0,1
+             m=i+j*L*(L-1)
+             v(m+N)=dble(j)*c
+          enddo
+          enddo
+      end subroutine moveBoundary
--- relax.f
+++ relax.f
@@ -6,43 +6,26 @@ C  [v lengths E dE] = opti_lbfgs(v,k,strain,kappa,L,N,f_c,eps)
 C======================================================================
 
 
-C     Gateway routine
       subroutine relax(N,L,v,k,strain,sts,eps)
-
-C     Declarations
       implicit none
 
-
-
-C     Arguments for computational routine:
       integer::N,L
 	  double precision,dimension(0:2*N-1)::v
 	  double precision, dimension(0:N-1,0:2) ::k
-	  
+
       real(8),dimension(0:2*N-1)::dE
       real(8),dimension(:,:),allocatable::w
       real(8)::strain,kappa,E,sts(0:1,0:1)
       real(8)::eps,xtol,f_c,eps_small
-
-      real(8),dimension(:),allocatable::work,diag
       integer::iprint(2),iflag,icall,i,j,imax,jmax,fmax
-
-      DOUBLE PRECISION GTOL,STPMIN,STPMAX
-      integer :: NN,M,MP,LP,indicator
-      logical::diagco
-      EXTERNAL LB2
-      COMMON /LB3/MP,LP,GTOL,STPMIN,STPMAX
+      integr::ir
 
       real(8)::dt,vv,ff,vf,f,acoef,mass
       real(8),dimension(:),allocatable::md_velocity
+      real(8)::u
       integer::nstep
 
-	  kappa = 0.d0
-        
-      iprint(1)=0
-      iprint(2)=0
-      icall = 0
-      iflag = 0
+	    kappa = 0.d0
       allocate(md_velocity(0:2*N-1))
       md_velocity(:) = 0.d0
       dt = 1.d-4
@@ -53,19 +36,21 @@ C     Arguments for computational routine:
       xtol = 1.d-15;
       eps_small = 1.d-10;
       NN = 2*N
-      M = 5
-	  
-      call Energy(v,k,kappa,L,strain,E,N)
-      call DEnergy(v,k,kappa,L,strain,dE,N,sts)
-	  
-	  	 
+      dE(:) =0.d0
 
-  51  CONTINUE
   20  CONTINUE
-      v = v + dt * md_velocity - 0.5 * dE * dt * dt / mass
-      md_velocity = md_velocity - 0.5 * dE * dt / mass
-      call DEnergy(v,k,kappa,L,strain,dE,N,sts)
-      md_velocity(0:2*N-1)=md_velocity(0:2*N-1)-0.5*dE(0:2*N-1)*dt/mass
+      moveBoundary(v,L,strain,N)
+      do ir = 1,N
+          call random_number(u)
+          i = int(N * u)
+          v(i) = v(i) + dt * md_velocity(ir) - 0.5 * dE(i) * dt * dt / mass
+          md_velocity(i) = md_velocity(i) - 0.5 * dE(i) * dt / mass
+          v(i+N)=v(i+N)+dt*md_velocity(i+N)-0.5*dE(i+N)*dt*dt/mass
+          md_velocity(i+N) = md_velocity(i+N) - 0.5 * dE(i+N) * dt / mass
+          call singleDEnergy(v,k,L,N,nsx,nsy,dE(i),dE(i+N))
+          md_velocity(i)=md_velocity(i)-0.5*dE(i)*dt/mass
+          md_velocity(i+N) = md_velocity(i+N) - 0.5*dE(i+N)*dt/mass
+      enddo
       if(maxval(dabs(dE(:))) .le. eps) go to 50
       vf = - DOT_PRODUCT(dE , md_velocity)
       vv = DOT_PRODUCT(md_velocity , md_velocity)
@@ -87,14 +72,10 @@ C     Arguments for computational routine:
      +                  (-acoef)*dsqrt(vv/ff)*dE(i)
         enddo
 
-    
-C      call LBFGS(NN,M,v,E,dE,diagco,diag,iprint,+
-C     +              eps_small,xtol,work,iflag)
-      icall = icall +1   
-C      if(iflag .lt. 0) go to 50   
+      icall = icall +1
       if(icall .gt. 800000) then
-        print *, 'not finished' 
-		stop       
+        print *, 'not finished'
+		stop
         go to 50
       endif
       go to 20
@@ -106,6 +87,3 @@ C      if(iflag .lt. 0) go to 50
 
       return
       end
-    
-     
-  
